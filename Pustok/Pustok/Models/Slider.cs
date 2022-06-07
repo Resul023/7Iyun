7bbcbdb8716177634e532c337673abf8dcc62790
Resul023 <73m6u7h@code.edu.az>
2022-06-07 01:00:20 -0700

    HomeWork

---
+++ Pustok/Pustok/Models/Slider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        
+        public string Desc { get; set; }
+        public string btnText { get; set; }
+        public string SliderImage { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+
+
+    }
+}
